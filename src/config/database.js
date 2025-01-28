e27840f1fb4171c8f98a5275679c03ce17b94a7a
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2025-01-28 10:47:47 -0500

    Actualizar configuración de Sequelize para usar DB_PASSWORD y agregar opciones de SSL

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Puedes ajustar esto según tus necesidades de seguridad
+        }
+    }
 });
 
 module.exports = sequelize;
