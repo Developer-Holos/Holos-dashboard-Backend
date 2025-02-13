0d4831eaa9007a2fb535946548c8a1b9a751e742
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2025-02-13 12:10:43 -0500

    Eliminar la configuración innecesaria en la instancia de OpenAIApi en el middleware

--- src/middleware/openai_config.js
+++ src/middleware/openai_config.js
@@ -1,11 +1,9 @@
-const { Configuration, OpenAIApi } = require('openai');
+const { OpenAIApi } = require('openai');
 
 const getOpenAIApiInstance = (apiKey) => {
-  const configuration = new Configuration({
+  return new OpenAIApi({
     apiKey: apiKey || process.env.OPENAI_API_KEY, // Usa la apiKey proporcionada o la de entorno
   });
-
-  return new OpenAIApi(configuration);
 };
 
 module.exports = getOpenAIApiInstance;
